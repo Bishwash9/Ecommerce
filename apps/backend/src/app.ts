40ffa007e4e5265aa9dcfd2a3f5226cb209e1c5d
Bishwas <bishwasmahajan@gmail.com>
2026-06-25 11:56:04 +0545

    -mfeat: added cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,15 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173', //frontend origin 
+    credentials: true //allow cookies to be sent from frontend
+}));
+
 app.use(express.json());
 app.use(cookieParser());
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the E-commerce API');
-});
 
 app.use('/api/users', userRoutes);
 
