e60ea2d1caf24262706babf1b946cf03d6fc65ea
Bishwas <bishwasmahajan@gmail.com>
2026-07-04 20:49:19 +0545

    feat(auth): add login page with authentication logic and layout component

---
+++ apps/web-users/src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AutLayout({ children}: {children: React.ReactNode}) {
+    
+    return (
+       
+            <div>  
+                    {children}
+            </div>
+        
+    );
+
+}
--- apps/web-users/src/app/layout.tsx
+++ apps/web-users/src/app/layout.tsx
@@ -1,4 +1,12 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import './globals.css'
+import { cookies } from 'next/headers'
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('accessToken')?.value;
+
+  //if cookies exist user is logged in 
+  const isLoggedIn = !!token;
     return (
         <html>
             <body>
