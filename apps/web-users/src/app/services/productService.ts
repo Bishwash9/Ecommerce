de827f4febf18e112a75ffc3a065aedf3f9a628e
Bishwas <bishwasmahajan@gmail.com>
2026-08-13 11:06:45 +0545

    refactor(productService): remove unused apiClient code and enhance product fetching logic making it server safe product service

--- apps/web-users/src/app/services/productService.ts
+++ apps/web-users/src/app/services/productService.ts
@@ -1,32 +1,57 @@
-import { apiClient } from '../config/api';
+// import { apiClient } from '../config/api';
 
-export const productService = { 
-    getAllProducts: async (): Promise<any> => {
-        const getData = await apiClient('products/fetch-products', {
-            method: 'GET'
-        }); 
+// export const productService = { 
+//     getAllProducts: async (): Promise<any> => {
+//         const getData = await apiClient('products/fetch-products', {
+//             method: 'GET'
+//         }); 
 
-        if(!getData.data){
-            console.error('Failed to fetch products');
-            throw new Error('Failed to fetch products');
-        }
+//         if(!getData.data){
+//             console.error('Failed to fetch products');
+//             throw new Error('Failed to fetch products');
+//         }
 
-        return getData.data;
-    },
+//         return getData.data;
+//     },
 
-    getProductById: async (id: string): Promise<any> => {
-        const getData = await apiClient(`products/fetch-product/${id}`, {
-            method: 'GET'
-        });
+//     getProductById: async (id: string): Promise<any> => {
+//         const getData = await apiClient(`products/fetch-product/${id}`, {
+//             method: 'GET'
+//         });
 
-        if(!getData.data){
-            console.error('Failed to fetch products');
-            throw new Error('Failed to fetch products');
-        }
+//         if(!getData.data){
+//             console.error('Failed to fetch products');
+//             throw new Error('Failed to fetch products');
+//         }
 
-        return getData.data
-    },
+//         return getData.data
+//     },
 
 
 
+// }
+
+import type { Product } from "../types/product";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5001/api";
+//server safe product service
+
+interface ProductResponse {
+    success: boolean;
+    data: Product[];
+}
+
+export async function getPublicProducts(): Promise<Product[]> {
+    const response = await fetch(`${BASE_URL}/products/fetch-products`, {
+        cache: 'no-store',
+    }
+);
+
+    if(!response.ok) {
+        throw new Error('Failed to fetch products');
+    }
+
+    const result = (await response.json()) as ProductResponse;
+
+    return result.data.filter(product => product.isActive); // Filter out inactive products
 }
