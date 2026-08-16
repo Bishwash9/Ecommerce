405e2ff95bab7627e671213b4cfbe1a4eef2e8d4
Bishwas <bishwasmahajan@gmail.com>
2026-08-16 18:48:31 +0545

    feat(cart): implement cart service with CRUD operations for managing user cart

---
+++ apps/web-users/src/app/services/cartService.ts
@@ -0,0 +1,60 @@
+import { apiClient } from "../config/api";
+
+export const cartService = {
+    addToCart: async (productId: string, quantity: number): Promise<any> => {
+
+        const addToCartData = await apiClient('/cart/add-cart', {
+            method: 'POST',
+            body: JSON.stringify({ productId, quantity }),
+        });
+
+        if (!addToCartData.ok) {
+            throw new Error(addToCartData.message || 'Failed to add product to cart');
+        }
+
+        return addToCartData.data;
+    },
+
+    getCart: async(): Promise<any> => {
+        
+        const getCartData = await apiClient('/cart/get-cart', {
+            method: 'GET'
+        });
+
+        if (!getCartData.ok) {
+            throw new Error(getCartData.message || 'Failed to get cart');
+        }
+
+        return getCartData.data;
+    },
+
+    removeFromCart: async (productId: string): Promise<any> => {
+
+        const removeFromCartData = await apiClient(`/cart/remove-cart/${productId}`, {
+            method: 'DELETE'
+        });
+
+        if (!removeFromCartData.ok) {
+            throw new Error(removeFromCartData.message || 'Failed to remove product from cart');
+        }
+
+        return removeFromCartData.data;
+
+    },
+
+    updateQuantity: async (productId: string, action: 'increment' | 'decrement'): Promise<any> => {
+
+        const updateQuantityData = await apiClient(`/cart/update-cart/${productId}`, {
+            method: 'PATCH',
+            body: JSON.stringify({ action })
+        });
+
+        if (!updateQuantityData.ok) {
+            throw new Error(updateQuantityData.message || 'Failed to update product quantity in cart');
+        }
+
+        return updateQuantityData.data;
+    }
+}
+
+
